4761598da5e62052cc8617373ab97ce1a9744c77
少主 <sungt2021@gamil.com>
2023-11-25 15:04:24 +0800

    First Commit

---
+++ Python-Learning/文件夹创建.py
@@ -0,0 +1,10 @@
+# import os
+# for day in range(1,120):
+#     path = 'D:\Code Files\Python\Python-Learning\Class-code'
+#     file_name=f'DAY {day}'
+#     path1=os.path.join(path,file_name)
+#     os.mkdir(path1)
+#     l1 = ['01 学习笔记.py','测试.py','21 期作业（爬虫作业）.py','23 期作业（68-71）.py','错难题整理.py']
+#     for j in l1:
+#         with open(f'D:\Code Files\Python\Python-Learning\Class-code\DAY {day}\{j}',encoding='utf-8',mode='w') as f:
+#             pass
